86bb402a30ad86c15b09c3f222a62ba1f63cf84d
himanshumps <himanshu.mps@gmail.com>
2019-01-03 13:15:33 +0530

    Missed $ sign

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -2,5 +2,5 @@
 
 echo "Running Jenkins JNLP Slave...."
 JAR=`ls -1 /opt/jenkins-slave/bin/slave.jar | tail -n 1`
-echo "java -jar slave.jar -headless -jar-cache $HOME -tunnel ${JENKINS_SERVICE_NAME}:${JENKINS_SERVICE_PORT} -url ${JENKINS_URL} {SECRET_KEY} {SLAVE_NAME}"
-exec java -jar $JAR -headless -jar-cache $HOME -tunnel ${JENKINS_SERVICE_NAME}:${JENKINS_SERVICE_PORT} -url ${JENKINS_URL} {SECRET_KEY} {SLAVE_NAME}
+echo "java -jar slave.jar -headless -jar-cache $HOME -tunnel ${JENKINS_TUNNEL_SERVICE_NAME}:${JENKINS_TUNNEL_SERVICE_PORT} -url ${JENKINS_URL} ${SECRET_KEY} ${SLAVE_NAME}"
+exec java -jar slave.jar -headless -jar-cache $HOME -tunnel ${JENKINS_TUNNEL_SERVICE_NAME}:${JENKINS_TUNNEL_SERVICE_PORT} -url ${JENKINS_URL} ${SECRET_KEY} ${SLAVE_NAME}
