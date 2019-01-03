711e53b9e8f6bca6bc393f1e93cac6e6919bfd99
himanshumps <himanshu.mps@gmail.com>
2019-01-03 14:20:09 +0530

    Modified to use jnlp file

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -2,5 +2,13 @@
 
 echo "Running Jenkins JNLP Slave...."
 JAR=`ls -1 /opt/jenkins-slave/bin/slave.jar | tail -n 1`
-echo "java -jar $JAR -headless -jar-cache $HOME -tunnel ${JENKINS_TUNNEL_SERVICE_NAME}:${JENKINS_TUNNEL_SERVICE_PORT} -url ${JENKINS_URL} ${SECRET_KEY} ${SLAVE_NAME}"
-exec java -jar $JAR -headless -jar-cache $HOME -tunnel ${JENKINS_TUNNEL_SERVICE_NAME}:${JENKINS_TUNNEL_SERVICE_PORT} -url ${JENKINS_URL} ${SECRET_KEY} ${SLAVE_NAME}
+# Get the JNLP file
+curl -o $HOME/agentJnlpFile.jnlp ${JNLP_URL}
+# Do the replacement
+echo "File before replacement"
+cat $HOME/agentJnlpFile.jnlp
+sed -i "s/${JENKINS_ROUTE_URL}/${JENKINS_SERVICE_URL}/g" test.jnlp $HOME/agentJnlpFile.jnlp
+echo "File after replacement"
+cat $HOME/agentJnlpFile.jnlp
+echo "java -jar $JAR -jnlpUrl \"file:$HOME/agentJnlpFile.jnlp\""
+java $JAR slave.jar -jnlpUrl "file:$HOME/agentJnlpFile.jnlp"
