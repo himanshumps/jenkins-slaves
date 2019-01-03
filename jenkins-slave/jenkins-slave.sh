acbcdb7e87a6b21166312304ce12398fd195fd08
himanshumps <himanshu.mps@gmail.com>
2019-01-03 13:09:23 +0530

    Changed to -jar approach

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-master_username=${JENKINS_USERNAME:-"admin"}
-master_password=${JENKINS_PASSWORD:-"password"}
-slave_executors=${EXECUTORS:-"1"}
 echo "Running Jenkins JNLP Slave...."
 JAR=`ls -1 /opt/jenkins-slave/bin/slave.jar | tail -n 1`
-TUNNEL="-tunnel ${JENKINS_TUNNEL}"
-URL="-url ${JENKINS_URL}"
-echo "java $JAVA_OPTS -cp $JAR hudson.remoting.jnlp.Main -headless $TUNNEL $URL -jar-cache $HOME"
-exec java $JAVA_OPTS -cp $JAR hudson.remoting.jnlp.Main -headless $TUNNEL $URL -jar-cache $HOME "$@"
+echo "java -jar slave.jar -headless -jar-cache $HOME -tunnel ${JENKINS_SERVICE_NAME}:${JENKINS_SERVICE_PORT} -url ${JENKINS_URL} {SECRET_KEY} {SLAVE_NAME}"
+exec java -jar $JAR -headless -jar-cache $HOME -tunnel ${JENKINS_SERVICE_NAME}:${JENKINS_SERVICE_PORT} -url ${JENKINS_URL} {SECRET_KEY} {SLAVE_NAME}
