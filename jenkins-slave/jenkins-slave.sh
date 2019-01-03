4e3c2b32b939243eb0aa3e51e9b3805a8341dd6f
himanshumps <himanshu.mps@gmail.com>
2019-01-03 12:26:22 +0530

    ok

--- jenkins-slave/jenkins-slave.sh
+++ jenkins-slave/jenkins-slave.sh
@@ -5,7 +5,7 @@ master_password=${JENKINS_PASSWORD:-"password"}
 slave_executors=${EXECUTORS:-"1"}
 echo "Running Jenkins JNLP Slave...."
 JAR=`ls -1 /opt/jenkins-slave/bin/slave.jar | tail -n 1`
-TUNNEL="-tunnel $JENKINS_TUNNEL"
-URL="-url $JENKINS_URL"
+TUNNEL="-tunnel ${JENKINS_TUNNEL}"
+URL="-url ${JENKINS_URL}"
 echo "java $JAVA_OPTS -cp $JAR hudson.remoting.jnlp.Main -headless $TUNNEL $URL -jar-cache $HOME"
 exec java $JAVA_OPTS -cp $JAR hudson.remoting.jnlp.Main -headless $TUNNEL $URL -jar-cache $HOME "$@"
